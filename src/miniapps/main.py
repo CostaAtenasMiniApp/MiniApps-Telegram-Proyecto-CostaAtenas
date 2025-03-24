68f7543ae7f61b73eef0c236cf81ffad5e43f649
Corso <58090876+yosbel-penate@users.noreply.github.com>
2025-03-24 00:30:27 -0400

    refractoring

--- src/miniapps/main.py
+++ src/miniapps/main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from EcecuteCommandBot import EcecuteCommandBot
 
-
 app = Flask(__name__, template_folder='template')
 
 @app.route('/classroom')
