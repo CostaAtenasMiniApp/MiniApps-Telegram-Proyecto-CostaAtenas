9d2978794effca539c46cc80cff83795eae3f69f
Corso <58090876+yosbel-penate@users.noreply.github.com>
2025-03-24 00:03:02 -0400

    change port

--- src/miniapps/main.py
+++ src/miniapps/main.py
@@ -22,4 +22,4 @@ def execute_command():
     return jsonify(result), status_code
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8081, debug=True)
