0e0649fb4820959f9ecdd52c90b3a3b1e3005afc
Corso <58090876+yosbel-penate@users.noreply.github.com>
2025-05-14 12:34:04 -0400

    main para produccion

--- src/apps/main.py
+++ src/apps/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .routes import register_routes
+from src.routes import register_routes  # Cambiado de .routes a src.routes
 import asyncio
 
 from src.core.services import StudentService
@@ -14,14 +14,18 @@ load_dotenv()
 app = Flask(__name__, template_folder='template')
 app.secret_key = os.getenv('FLASK_SECRET_KEY', '18d8791438b46514da6b131e360a2392')
 
-# Mueve la inicialización a una función aparte
 async def init_app():
     await init_db()
     student_repository = TortoiseStudentRepository()
     student_service = StudentService(student_repository)
     register_routes(app, student_service)
 
-# Esto asegura que `app` esté disponible para PythonAnywhere
-if __name__ == "__main__":
+# Configuración para producción
+def create_app():
+    """Factory para crear la aplicación (necesario para WSGI)"""
     asyncio.run(init_app())
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
