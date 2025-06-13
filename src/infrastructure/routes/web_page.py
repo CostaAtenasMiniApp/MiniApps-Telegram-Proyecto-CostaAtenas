7291a8f05c68dfa0d8b534fbb36d1ab1f41660d8
saronzdev <ronaldsrd@protonmail.com>
2025-06-13 16:03:11 -0400

    new web fully added

--- src/infrastructure/routes/web_page.py
+++ src/infrastructure/routes/web_page.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates 
-import config
+from src.shared import config 
 
 router = APIRouter()
 
